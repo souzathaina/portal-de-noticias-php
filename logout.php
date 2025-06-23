d07dac4c1d8453ca096bdbea3c5444726c64fdc0
Gustavokvs <gustavokruger514@gmail.com>
2025-06-23 19:17:35 -0300

    Ajustes

--- logout.php
+++ logout.php
@@ -3,6 +3,9 @@ session_start();
 session_unset(); // Remove todas as variáveis de sessão
 session_destroy(); // Encerra a sessão completamente
 
+
+setcookie("usuarioLogin", $usuario, time() - 3600);
+
 // Redireciona o usuário para a página de login
 header("Location: login.php");
 exit;
